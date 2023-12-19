52ae5dbe831504873cf2ed270523d12dd2a79366
Marie <mje.espinosa@gmail.com>
2023-12-19 14:11:06 +0100

    use .env

--- backend/env.ts
+++ backend/env.ts
@@ -21,4 +21,14 @@ export default Env.rules({
 	APP_NAME: Env.schema.string(),
  	DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+
+
+	DB_CONNECTION: Env.schema.string(),
+	DB_HOST: Env.schema.string(),
+	DB_PORT: Env.schema.number(),
+	DB_USER: Env.schema.string(),
+	DB_PASSWORD: Env.schema.string(),
+	DB_DATABASE: Env.schema.string(),
+
+
 })
